f425b6d56980a5798ce86962e55eb8599fc00d3c
Rohit Sopan Najan <najanrohitsopan@gmail.com>
2026-08-16 22:19:34 +0530

    Update Main.java

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -1,12 +1,5 @@
 package project;
-
-import project.app.RoadGuardianApp;
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Application.launch(RoadGuardianApp.class,args);
+ 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
